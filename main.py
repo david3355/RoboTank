b4cc6ffc26aad93f8a38b5fcf1dfeab3e8727985
David Bakos <david3355@gmail.com>
2020-11-03 18:33:25 +0100

    Handle unix signals for safe exit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Jager's RoboTank Project
+import signal
 
 from motor.driver import MotorDriver
 from network.discovery.broadcaster import Broadcaster
@@ -7,27 +8,46 @@ from server.control_server import ControlServer
 from server.processor import CommandProcessor
 
 
-if __name__ == '__main__':
-    broadcaster = Broadcaster(broadcast_interval_sec=2)
-    # wlan_interface_name="enp0s3" For virtualbox test
-    broadcaster.start_broadcasting()
+broadcaster = Broadcaster(broadcast_interval_sec=2)  # wlan_interface_name="enp0s3" For virtualbox test
+command_server = CommandServer(25500)
+control_server = ControlServer(25000)
 
+
+def stop_services():
+    try:
+        command_server.stop()
+        control_server.stop()
+        broadcaster.stop_broadcasting()
+    except BaseException as bex:
+        print("Error on exit: {}".format(bex))
+
+
+def handle_term_signals(signum, frame):
+    print("Handling signal: {}, {}".format(signum, frame))
+    stop_services()
+
+
+def wait_for_user():
+    inp = None
+    while inp != "x":
+        inp = input("Press x to close servers")
+    stop_services()
+
+
+if __name__ == '__main__':
+
+    signal.signal(signal.SIGINT, handle_term_signals)
+    signal.signal(signal.SIGTERM, handle_term_signals)
+
+    broadcaster.start_broadcasting()
     driver = MotorDriver()
     cmd_processor = CommandProcessor(driver)
 
     CommandHandler.set_driver(driver)
     CommandHandler.set_command_processor(cmd_processor)
-    command_server = CommandServer(25500)
-    command_server.start()
 
-    control_server = ControlServer(25000)
+    command_server.start()
     control_server.add_command_processor(cmd_processor)
     control_server.start()
 
-    inp = None
-    while inp != "x":
-        inp = input("Press x to close servers")
-
-    command_server.stop()
-    control_server.stop()
-    broadcaster.stop_broadcasting()
+    #wait_for_user()
