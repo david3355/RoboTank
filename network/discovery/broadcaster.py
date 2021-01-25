48f559c8f4f54fcd7565b94f5513448ea7536357
David Bakos <david3355@gmail.com>
2021-01-25 19:31:39 +0100

    Broadcast addresses on all wlan interfaces

--- network/discovery/broadcaster.py
+++ network/discovery/broadcaster.py
@@ -2,13 +2,13 @@ import socket
 from threading import Thread
 from time import sleep
 
-from network.discovery.network_interface_helper import get_ip, RASPBERRY_WLAN_INTERFACE
+from network.discovery.network_interface_helper import get_ip, get_interfaces, get_broadcast
 
 
 class Broadcaster:
     BROADCAST_PORT = 25555
 
-    def __init__(self, broadcast_interval_sec=1, wlan_interface_name=RASPBERRY_WLAN_INTERFACE):
+    def __init__(self, broadcast_interval_sec=1):
         self.remote_port = self.BROADCAST_PORT
         self.socket = socket.socket(socket.AF_INET,  # Internet
                                     socket.SOCK_DGRAM)
@@ -16,19 +16,20 @@ class Broadcaster:
         self.broadcasting = False
         self.caster = Thread(target=self.__sender, daemon=True)
         self.sleep_time = broadcast_interval_sec
-        self.wlan_interface = wlan_interface_name
+        self.interfaces = get_interfaces(name_filter="wlan")
 
-
-    def __send(self, robotank_ip):
-        print("Broadcasting RoboTank IP [{}] to port {}".format(robotank_ip, self.remote_port))
+    def __send(self, robotank_ip, interface):
+        dest_addr = get_broadcast(interface)
+        print("Broadcasting RoboTank IP [{}] to port {} on {}[{}]".format(robotank_ip, self.remote_port, interface, dest_addr))
         #self.socket.sendto(robotank_ip, ("<broadcast>", self.remote_port))
-        self.socket.sendto(robotank_ip.encode('utf-8'), ("<broadcast>", self.remote_port))
+        self.socket.sendto(robotank_ip.encode('utf-8'), (dest_addr, self.remote_port))
 
     def __sender(self):
         while self.broadcasting:
             try:
-                ip = get_ip(self.wlan_interface)
-                self.__send(ip)
+                for iface in self.interfaces:
+                    ip = get_ip(iface)
+                    self.__send(ip, iface)
                 sleep(self.sleep_time)
             except BaseException as ip_exception:
                 print("Cannot broadcast RoboTank IP: ".format(ip_exception))
--- network/discovery/network_interface_helper.py
+++ network/discovery/network_interface_helper.py
@@ -6,3 +6,13 @@ RASPBERRY_WLAN_INTERFACE = "wlan0"
 def get_ip(interface):
     ip = ni.ifaddresses(interface)[ni.AF_INET][0]['addr']
     return ip
+
+
+def get_broadcast(interface):
+    ip = ni.ifaddresses(interface)[ni.AF_INET][0]['broadcast']
+    return ip
+
+
+def get_interfaces(name_filter=""):
+    interfaces = ni.interfaces()
+    return [iface for iface in interfaces if name_filter in iface]
